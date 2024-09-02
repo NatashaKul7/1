13ca31ceb6b41da7654e086d1c9aad6fb4eb1135
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2024-09-02 19:34:05 +0200

    add, delete, edit

---
+++ todo/src/components/EditTodoForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+const EditTodoForm = ({task, editTodo}) => {
+  const [value, setValue] = useState(task.task);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    editTodo(value, task.id);
+  };
+
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={value}
+        className="todo-input"
+        placeholder="Update Task"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        Update Task
+      </button>
+    </form>
+  )
+}
+
+export default EditTodoForm
