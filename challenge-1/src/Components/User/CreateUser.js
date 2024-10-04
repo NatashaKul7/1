5c31b06ea703cefdafe20757cf04a0b7d63889f2
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2024-10-04 11:45:33 +0200

    refs, portals

--- challenge-1/src/Components/User/CreateUser.js
+++ challenge-1/src/Components/User/CreateUser.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import Button from "../UI/Button";
 import Card from "../UI/Card";
@@ -6,14 +6,20 @@ import styles from "./CreateUser.module.css";
 import ErrorModal from "../UI/ErrorModal";
 
 const CreateUser = (props) => {
-  const [inputName, setInputName] = useState("");
-  const [inputAge, setInputAge] = useState("");
+  // const [inputName, setInputName] = useState('');
+  // const [inputAge, setInputAge] = useState('');
   const [error, setError] = useState();
+
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
   const createUserHandler = (e) => {
     e.preventDefault();
 
-    if (inputName.trim().length === 0 || inputAge.trim().length === 0) {
+    const inputUserName = nameInputRef.current.value
+    const inputUserAge = ageInputRef.current.value
+
+    if (inputUserName.trim().length === 0 || inputUserAge.trim().length === 0) {
       setError({
         title: "Incorrect input",
         message: "These fields can not be empty",
@@ -21,7 +27,7 @@ const CreateUser = (props) => {
       return;
     }
 
-    if (+inputAge < 1) {
+    if (+inputUserAge < 1) {
       setError({
         title: "Incorrect age",
         message: "Age must be greater than 0",
@@ -29,42 +35,51 @@ const CreateUser = (props) => {
       return;
     }
 
-    props.onCreateUser(inputName, inputAge);
+    props.onCreateUser(inputUserName, inputUserAge);
+        
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
 
-    setInputName("");
-    setInputAge("");
   };
 
-  const nameChangeHandler = (e) => {
-    setInputName(e.target.value);
-  };
+  // const nameChangeHandler = (e) => {
+  //   setInputName(e.target.value);
+  // };
 
-  const ageChangeHandler = (e) => {
-    setInputAge(e.target.value);
-  };
+  // const ageChangeHandler = (e) => {
+  //   setInputAge(e.target.value);
+  // };
 
   const errorHandler = () => {
-    setError(false)
-  }
+    setError(false);
+  };
 
   return (
     <>
-      {error && <ErrorModal title={error.title} message={error.message} onCloseModal={errorHandler}/>}
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onCloseModal={errorHandler}
+        />
+      )}
       <Card className={styles.input}>
         <form onSubmit={createUserHandler}>
           <label htmlFor="name">Name</label>
           <input
             id="name"
             type="text"
-            value={inputName}
-            onChange={nameChangeHandler}
+            // value={inputName}
+            ref={nameInputRef}
+            // onChange={nameChangeHandler}
           />
           <label htmlFor="age">Age</label>
           <input
             id="age"
             type="number"
-            value={inputAge}
-            onChange={ageChangeHandler}
+            // value={inputAge}
+            ref={ageInputRef}
+            // onChange={ageChangeHandler}
           />
           <Button type="submit">Add user</Button>
         </form>
