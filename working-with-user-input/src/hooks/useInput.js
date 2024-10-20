063443a84ddc59eddce8406434ec00e76d726ce2
NatashaKul7 <natasha.kulibaba0703@gmail.com>
2024-10-20 16:43:37 +0300

    +reducer

--- working-with-user-input/src/hooks/useInput.js
+++ working-with-user-input/src/hooks/useInput.js
@@ -1,32 +1,78 @@
-import { useState } from "react";
+import {
+  useReducer,
+  //  useState
+} from "react";
+
+const initialInputState = {
+  inputValue: "",
+  wasTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT_CHANGE") {
+    return {
+      inputValue: action.value,
+      wasTouched: state.wasTouched,
+    };
+  }
+
+  if (action.type === "INPUT_BLUR") {
+    return {
+      inputValue: state.inputValue,
+      wasTouched: true,
+    };
+  }
+
+  if (action.type === "RESET_INPUT") {
+    return {
+      inputValue: "",
+      wasTouched: false,
+    };
+  }
+
+  return initialInputState;
+};
 
 const useInput = (validateValueFunc) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [wasInputTouched, setWasInputTouched] = useState(false);
+  const [inputState, dispatchAction] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
 
-  const isValueValid = validateValueFunc(enteredValue);
-  const isInputInvalid = !isValueValid && wasInputTouched;
+  //   const [enteredValue, setEnteredValue] = useState("");
+  //   const [wasInputTouched, setWasInputTouched] = useState(false);
+
+  const isValueValid = validateValueFunc(inputState.inputValue);
+  const isInputInvalid = !isValueValid && inputState.wasTouched;
 
   const inputChangeHandler = (e) => {
-    setEnteredValue(e.target.value);
+    dispatchAction({ type: "INPUT_CHANGE", value: e.target.value });
+
+    // setEnteredValue(e.target.value);
   };
 
   const inputLostFocusHandler = (e) => {
-    setWasInputTouched(true);
+    dispatchAction({ type: "INPUT_BLUR", value: e.target.value });
+
+    // setWasInputTouched(true);
   };
 
   const resetValues = () => {
-    setEnteredValue('')
-    setWasInputTouched(false)
-  }
+    dispatchAction({ type: "RESET_INPUT" });
+
+    // setEnteredValue("");
+    // setWasInputTouched(false);
+  };
 
   return {
-    value: enteredValue,
+    // value: enteredValue,
+
+    value: inputState.inputValue,
     hasError: isInputInvalid,
     isValid: isValueValid,
     inputChangeHandler,
     inputLostFocusHandler,
-    resetValues
+    resetValues,
   };
 };
 
